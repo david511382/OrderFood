4010dfa0529c8d16e6dea9c9cd7bf34e84cd00c9
david511382 <david511382@yahoo.com.tw>
2019-06-05 13:27:36 +0800

    db member update delete test

--- src/database/mysql/member_test.go
+++ src/database/mysql/member_test.go
@@ -9,22 +9,22 @@ import (
 )
 
 var (
-	memberOutput = models.Member{
+	dbMember = models.Member{
 		ID:       1,
 		Name:     "test name",
 		Username: "test username",
 		Password: "test password",
 	}
+	memberInput = models.Member{
+		Name:     dbMember.GetName(),
+		Username: dbMember.GetUsername(),
+		Password: dbMember.GetPassword(),
+	}
 )
 
 func TestAddMember(t *testing.T) {
-	output1 := memberOutput
-	member := models.Member{
-		ID:       0,
-		Name:     memberOutput.GetName(),
-		Username: memberOutput.GetUsername(),
-		Password: memberOutput.GetPassword(),
-	}
+	output1 := dbMember
+	member := memberInput
 
 	flagtests := []struct {
 		name   string
@@ -50,8 +50,11 @@ func TestAddMember(t *testing.T) {
 		})
 	}
 }
-
 func TestGetMember(t *testing.T) {
+	member := memberInput
+	err := memberDb.AddMember(&member)
+	assert.Nil(t, err)
+
 	flagtests := []struct {
 		name   string
 		input  *models.Member
@@ -61,41 +64,41 @@ func TestGetMember(t *testing.T) {
 		{
 			name: "get 1 id",
 			input: &models.Member{
-				ID: memberOutput.GetID(),
+				ID: dbMember.GetID(),
 			},
 			err: nil,
 			output: []models.Member{
-				memberOutput,
+				dbMember,
 			},
 		},
 		{
 			name: "get 1 name",
 			input: &models.Member{
-				Name: memberOutput.GetName(),
+				Name: dbMember.GetName(),
 			},
 			err: nil,
 			output: []models.Member{
-				memberOutput,
+				dbMember,
 			},
 		},
 		{
 			name: "get 1 username",
 			input: &models.Member{
-				Username: memberOutput.GetUsername(),
+				Username: dbMember.GetUsername(),
 			},
 			err: nil,
 			output: []models.Member{
-				memberOutput,
+				dbMember,
 			},
 		},
 		{
 			name: "get 1 password",
 			input: &models.Member{
-				Password: memberOutput.GetPassword(),
+				Password: dbMember.GetPassword(),
 			},
 			err: nil,
 			output: []models.Member{
-				memberOutput,
+				dbMember,
 			},
 		},
 	}
@@ -104,6 +107,131 @@ func TestGetMember(t *testing.T) {
 		t.Run(flag.name, func(t *testing.T) {
 			input := *flag.input
 			output, err := memberDb.GetMember(&input)
+			assert.Equal(t, flag.err, err)
+			assert.Equal(t, flag.output, output)
+		})
+	}
+}
+func TestUpdateMember(t *testing.T) {
+	member := memberInput
+	err := memberDb.AddMember(&member)
+	assert.Nil(t, err)
+
+	newMember := &models.Member{
+		ID:       dbMember.GetID(),
+		Name:     "new name",
+		Username: "new username",
+		Password: "new password",
+	}
+
+	flagtests := []struct {
+		name   string
+		input  models.Member
+		err    error
+		output int64
+	}{
+		{
+			name: "update 1 name",
+			input: models.Member{
+				ID:   dbMember.GetID(),
+				Name: newMember.GetName(),
+			},
+			err:    nil,
+			output: 1,
+		},
+		{
+			name: "update 1 username",
+			input: models.Member{
+				ID:       dbMember.GetID(),
+				Username: newMember.GetUsername(),
+			},
+			err:    nil,
+			output: 1,
+		},
+		{
+			name: "update 1 password",
+			input: models.Member{
+				ID:       dbMember.GetID(),
+				Password: newMember.GetPassword(),
+			},
+			err:    nil,
+			output: 1,
+		},
+		{
+			name:   "update 1",
+			input:  member,
+			err:    nil,
+			output: 1,
+		},
+	}
+
+	for _, flag := range flagtests {
+		t.Run(flag.name, func(t *testing.T) {
+			input := flag.input
+			inputp := &input
+			output, err := memberDb.UpdateMember(inputp)
+			assert.Equal(t, flag.err, err)
+			assert.Equal(t, flag.output, output)
+		})
+	}
+}
+func TestDeleteMember(t *testing.T) {
+	member := memberInput
+
+	flagtests := []struct {
+		name   string
+		input  models.Member
+		err    error
+		output int64
+	}{
+		{
+			name: "delete 1 id",
+			input: models.Member{
+				ID:       dbMember.GetID(),
+			},
+			err:    nil,
+			output: 1,
+		},
+		{
+			name: "delete 1 name",
+			input: models.Member{
+				Name: dbMember.GetName(),
+			},
+			err:    nil,
+			output: 1,
+		},
+		{
+			name: "delete 1 username",
+			input: models.Member{
+				Username: dbMember.GetUsername(),
+			},
+			err:    nil,
+			output: 1,
+		},
+		{
+			name: "delete 1 password",
+			input: models.Member{
+				Password: dbMember.GetPassword(),
+			},
+			err:    nil,
+			output: 1,
+		},
+		{
+			name:   "delete 1",
+			input:  dbMember,
+			err:    nil,
+			output: 1,
+		},
+	}
+
+	for _, flag := range flagtests {
+		t.Run(flag.name, func(t *testing.T) {
+			err := memberDb.AddMember(&member)
+			assert.Nil(t, err)
+
+			input := flag.input
+			inputp := &input
+			output, err := memberDb.DeleteMember(inputp)
 			assert.Equal(t, flag.err, err)
 			assert.Equal(t, flag.output, output)
 		})
